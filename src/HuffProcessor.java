a68246e70ab6496bd5b925cd592d3a94be1ccafc
Shreyas Kulkarni <srk37@duke.edu>
2018-12-07 20:28:24 -0500

    wefwef

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -60,7 +60,7 @@ public class HuffProcessor {
 	 */
 	public void decompress(BitInputStream in, BitOutputStream out){
 
-		int val = in.readBits(BITS_PER_WORD);
+		int val = in.readBits(BITS_PER_INT);
 //		if (val == -1) break;
 		if (val != HUFF_TREE) { 
 			throw new HuffException("illegal header starts with " +val); 
